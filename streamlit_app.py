9560152490e65027cb904cd6e3850f15ec2d978e
yunqiqiliang <132561395+yunqiqiliang@users.noreply.github.com>
2023-05-21 23:11:01 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 from apps import home, heatmap, upload  # import your app modules here
 
-st.set_page_config(page_title="Streamlit Geospatial", layout="wide")
+st.set_page_config(page_title="Play Your Data With Lakehouse", layout="wide")
 
 # A dictionary of apps in the format of {"App title": "App icon"}
 # More icons can be found here: https://icons.getbootstrap.com
@@ -11,6 +11,11 @@ apps = [
     {"func": home.app, "title": "Home", "icon": "house"},
     {"func": heatmap.app, "title": "Heatmap", "icon": "map"},
     {"func": upload.app, "title": "Upload", "icon": "cloud-upload"},
+    {"func": home.app, "title": "连接数据集", "icon": "house"},
+    {"func": heatmap.app, "title": "数据档案", "icon": "map"},
+    {"func": upload.app, "title": "数据探查", "icon": "cloud-upload"},
+    {"func": home.app, "title": "数据处理", "icon": "house"},
+    {"func": heatmap.app, "title": "数据分析", "icon": "map"},
 ]
 
 titles = [app["title"] for app in apps]
